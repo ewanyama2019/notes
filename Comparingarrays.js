1eaaa82fb16671b20b48b7c088ca1d90cbd115f0
Eric Wanyama <wafulawanyama@gmail.com>
2019-03-28 09:26:16 +0300

    add more array.slice examples

--- Comparingarrays.js
+++ Comparingarrays.js
@@ -21,3 +21,23 @@ cloneArray    ////expected [1,2,3,4,5,6,7,8,9]
 cloneArray.push(10,11,12)
 cloneArray     //expected  [1,2,3,4,5,6,7,8,9,10,11,12]
 originalArray  //expected [1,2,3,4,5,6,7,8,9]
+
+newCloneArray2 = cloneArray.slice(4)  //Clone the cloneArray from place holder 4
+skipCloneArray = cloneArray.slice(4, 8)  ///clone the cloneArray from place holder 4 to 8 excludingthe 8th
+
+
+var cloneArray = originalArray.slice()
+cloneArray
+cloneArray.push(10,11,12)
+cloneArray
+originalArray
+newCloneArray2 = cloneArray.slice(4)
+skipCloneArray = cloneArray.slice(4, 8)
+largerCloneArray = cloneArray.slice(1,7);
+
+var animals = ['ant', 'bison', 'camel', 'duck', 'elephant', 'giraffe'];
+animals
+animals.slice()
+animals.slice(2);
+animals.slice(2, 4);
+animals.slice(2,7);
