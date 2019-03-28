4cc927921b411c4b1418701f4323da36a6a74ece
Eric Wanyama <wafulawanyama@gmail.com>
2019-03-28 13:11:02 +0300

    Adding Numbers with Loops with Array.forEach() method

--- wednesday_code_practice1.js
+++ wednesday_code_practice1.js
@@ -8,3 +8,29 @@ parentsnMovieChar.push('Tezzie');  //Add your pets' names using push().
 parentsnMovieChar; //parentsnMovieChar n Pet name
 reverseParentsnMovieChar = parentsnMovieChar.reverse() //Reverse the order of the array.
 parents[0]; //Access one of your parents' names using the square bracket notation
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+//looping in Arrays
+var languages = ['HTML','CSS','JavaScript'];
+  languages.forEach(function(language){
+    alert('I love ' + language + '!');
+  })
+
+  //Adding Numbers with Loops
+  var total =0;
+  var summands = [1,2,3,4,5];
+  summands.forEach(function(summand){
+    total += summand;
+  });
+  alert(total)
+
+
+//Adding Numbers with Loops inproved code
+var total =0;
+var summands = [1,2,3,4,5];
+summands.forEach(function(summand){
+  alert(total += summand)  //alert the subtotals
+});
+alert(total)   //the final total
