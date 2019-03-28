6b6555f601082d078541e98a4ea64c7c2ba17c8e
Eric Wanyama <wafulawanyama@gmail.com>
2019-03-28 12:41:41 +0300

    add Wednesday Practice -Javascript Arrays

---
+++ wednesday_code_practice1.js
@@ -0,0 +1,10 @@
+///### Wednesday: Practice - JavaScript Arrays ####
+
+var movieChar =['Thor','Bumblebee','Optimus','robotboy','Nikita']; //Make an array of your siblings' names or your favorite movie characters' names.
+var parents = ['Francis','Flo'];  //Make an array of your parents' names.
+var parentsnMovieChar = parents.concat(movieChar); //Combine these two arrays using concat().
+parentsnMovieChar
+parentsnMovieChar.push('Tezzie');  //Add your pets' names using push().
+parentsnMovieChar; //parentsnMovieChar n Pet name
+reverseParentsnMovieChar = parentsnMovieChar.reverse() //Reverse the order of the array.
+parents[0]; //Access one of your parents' names using the square bracket notation
